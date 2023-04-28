3ef17beec6913f304cc5e4051f4caab073811820
Hatlestad <lincolnh@iastate.edu>
2023-04-27 21:00:29 -0500

    finished sound methods

--- roballers/sound.c
+++ roballers/sound.c
@@ -1,5 +1,27 @@
 #include "sound.h"
 
-void sound_play_song(int time) {
 
+//loads song index of choice to the oi
+void sound_load_songs() {
+    //Never Gonna Give You UP
+    int rickrollNumNotes = 11;
+    unsigned char rickrollNotes[11]    = {53, 55, 48, 55, 57, 60, 58, 57, 53, 55, 48};
+    unsigned char rickrollDuration[11] = {48, 64, 16, 48, 48, 8,  8,  8,  48, 64, 64};
+    oi_loadSong(RICK_ROLL, rickrollNumNotes, rickrollNotes, rickrollDuration);
+
+    //Jaws Theme
 }
+
+//plays a song of choice numLoops times
+void sound_play_song(int songIndex, int numLoops) {
+
+    int n; //loop iterator
+
+    while(n < numLoops) {
+        timer_waitMillis(8000);
+        oi_play_song(songIndex);
+        n++;
+    }
+}
+
+
--- roballers/sound.h
+++ roballers/sound.h
@@ -1,7 +1,25 @@
 #ifndef SOUND_H_
 #define SOUND_H_
 
+#define RICK_ROLL   0
+//#define IMERPIAL_MARCH      1
+//#define MARIO_UNDERWORLD    3
+//#define MARIO_UNDERWATER    7
+#define JAWS    1
+
+
+//include these lines for for proper use
+//    oi_t *sensor_data = oi_alloc();
+//    oi_init(sensor_data);
+
+
+
 //plays loaded song for specified amount of time
-void sound_play_song(int time);
+void sound_load_song(int time);
+
+//plays a song of choice numLoops times
+
+void sound_play_song(int songIndex, int numLoops);
+
 
 #endif
